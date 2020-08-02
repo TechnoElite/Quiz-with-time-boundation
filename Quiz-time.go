48df8a1273b7194cd7e72af3229d37a3fab34ce2
Rohit <khanderaorohit181@gmail.com>
2020-08-02 14:19:08 +0530

    my first commit using git

---
+++ Quiz-time.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	csvFilename := flag.String("csv", "test.csv", "a csv file in the format of 'question,answer'")
+	timeLimit := flag.Int("limit", 10, "the time limit for the quiz in seconds")
+	flag.Parse()
+
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFilename))
+	}
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse the provided CSV file.")
+	}
+	problems := parseLines(lines)
+
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+	correct := 0
+	rand.Seed(time.Now().UnixNano())
+	var uniques []int
+
+	for i := range problems {
+
+	sloop:
+		z := rand.Intn(len(problems))
+		for _, u := range uniques {
+			if u == z {
+				goto sloop
+			}
+		}
+		uniques = append(uniques, z)
+		fmt.Printf("Problem #%d: %s = ", i+1, problems[z].q)
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Println()
+			fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
+			return
+		case answer := <-answerCh:
+			if answer == problems[z].a {
+				correct++
+			}
+		}
+	}
+
+	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
